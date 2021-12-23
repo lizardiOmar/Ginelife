b0f4dab92ea5ee6ec3c0b5e2429173e620d3668b
lizardiOmar <71732399+lizardiOmar@users.noreply.github.com>
2021-12-23 11:56:50 -0600

    Clave de acceso

--- SQL/Doctoras_Pacientes_FK.sql
+++ SQL/Doctoras_Pacientes_FK.sql
@@ -1,14 +1,15 @@
 CREATE SCHEMA `ginelife` ;
 use ginelife;
 /*TABLA DE LAS ESPECIALISTAS*/
-CREATE TABLE `ginelife`.`doctoras` (
- `idDOCTORAS` INT NOT NULL AUTO_INCREMENT,
- `Nombres` VARCHAR(55) NOT NULL,
- `Apellidos` VARCHAR(55) NOT NULL,
- `CedulaProfesional` VARCHAR(15) NOT NULL,
- `CedulaEspecialidad` VARCHAR(15) NOT NULL,
- `Telefono` VARCHAR(25) NOT NULL,
- `Email` VARCHAR(55) NOT NULL,
+CREATE TABLE `doctoras` (
+ `idDOCTORAS` int(11) NOT NULL AUTO_INCREMENT,
+ `Nombres` varchar(55) NOT NULL,
+ `Apellidos` varchar(55) NOT NULL,
+ `CedulaProfesional` varchar(15) NOT NULL,
+ `CedulaEspecialidad` varchar(15) NOT NULL,
+ `Telefono` varchar(25) NOT NULL,
+ `Email` varchar(55) NOT NULL,
+ `Clave` varchar(25) NOT NULL,
  PRIMARY KEY (`idDOCTORAS`));
 /*TABLA DE LAS PACIENTES/ficha de identificación*/
 CREATE TABLE `ginelife`.`pacientes` (
@@ -24,4 +25,3 @@ CREATE TABLE `ginelife`.`pacientes` (
  PRIMARY KEY (`idPacientes`));
 ALTER TABLE pacientes
 ADD FOREIGN KEY (idDoctora) REFERENCES doctoras(idDoctoras);
-
