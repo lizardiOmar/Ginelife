47eb911641c6b622cdb2323d55c995c2ea73de96
lizardiOmar <71732399+lizardiOmar@users.noreply.github.com>
2021-12-23 12:04:04 -0600

    Domicilios
    -Tabla domicilosPacientes
    -FK idPacientes

---
+++ SQL/Pacientes_Domicilios_FK.sql
@@ -0,0 +1,13 @@
+/*DOMICILIO DE LAS PACIENTES*/
+use ginelife;
+CREATE TABLE `ginelife`.`domiciliopaciente` (
+  `idDomicilioPaciente` INT NOT NULL,
+  `Municipio` VARCHAR(55) NOT NULL,
+  `Colonia` VARCHAR(55) NOT NULL,
+  `Calle` VARCHAR(55) NOT NULL,
+  `Numero` VARCHAR(5) NOT NULL,
+  `CP` VARCHAR(10) NOT NULL,
+  `idPaciente` INT NOT NULL,
+  PRIMARY KEY (`idDomicilioPaciente`));
+ALTER TABLE domicilioPaciente
+ADD FOREIGN KEY (idPaciente) REFERENCES pacientes(idPacientes);
