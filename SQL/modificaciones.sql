d371aae05791770988815a91a1a068c59b3d537f
lizardiOmar <71732399+lizardiOmar@users.noreply.github.com>
2021-12-28 16:47:00 -0600

    Update modificaciones.sql

--- SQL/modificaciones.sql
+++ SQL/modificaciones.sql
@@ -1,3 +1,6 @@
 ALTER TABLE `ginelife`.`antecedentespersonalestraumaticos` 
 ADD COLUMN `YYYY` VARCHAR(5) NOT NULL AFTER `Descripcion`,
-ADD COLUMN `idHhistoriaClinica` INT NOT NULL AFTER `YYYY`;
+ADD COLUMN `idHistoriaClinica` INT NOT NULL AFTER `YYYY`;
+use ginelife;
+ALTER TABLE antecedentespersonalestraumaticos
+ADD FOREIGN KEY (idHistoriaClinica) REFERENCES HistoriaClinica(idHistoriaClinica);
